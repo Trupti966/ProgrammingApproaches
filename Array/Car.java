bcab32c8d95e8a1b764ef71cdfe07d1fcbac665c
Truptimayee Barik <85446034+Trupti966@users.noreply.github.com>
2022-08-27 10:03:55 +0530

    All Concepts of Array Data Structure

---
+++ Array/Car.java
@@ -0,0 +1,17 @@
+class Car 
+{
+	String name;
+	int cost;
+}
+class Test
+{
+	public static void main(String[] args) {
+		Car x = new Car();
+		x.name = "Maruthi";
+		x.cost = 200000;
+		
+		System.out.println(x.name);
+		System.out.println(x.cost);
+			
+	}
+}
